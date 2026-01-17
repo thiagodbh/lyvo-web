e00d07423936b3e4752ae96dbee989cdaad952d1
thiagodbh <thiagodbh@gmail.com>
2026-01-17 11:15:48 -0300

    Create firebase.ts

---
+++ services/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMjEvI1iLTMXMxYPfaB6-042hs8bApK2k",
+  authDomain: "lyvo-web.firebaseapp.com",
+  projectId: "lyvo-web",
+  storageBucket: "lyvo-web.appspot.com",
+  messagingSenderId: "501648718670",
+  appId: "1:501648718670:web:cc5a8e6e2119cc8222bc1f",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
