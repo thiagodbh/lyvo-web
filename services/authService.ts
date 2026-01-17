09faf048692a1c6ba8bac3092177a04b0e9cd51c
thiagodbh <thiagodbh@gmail.com>
2026-01-17 11:20:02 -0300

    Create authService.ts

---
+++ services/authService.ts
@@ -0,0 +1,26 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  User,
+} from "firebase/auth";
+import { auth } from "./firebase";
+
+export const authService = {
+  signUp(email: string, password: string) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  },
+
+  signIn(email: string, password: string) {
+    return signInWithEmailAndPassword(auth, email, password);
+  },
+
+  signOut() {
+    return signOut(auth);
+  },
+
+  onChange(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback);
+  },
+};
