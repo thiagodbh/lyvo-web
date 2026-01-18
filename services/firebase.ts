66b3764ad8ddc986ccb8e16d5390b3e084a0bab9
thiagodbh <thiagodbh@gmail.com>
2026-01-18 14:43:11 -0300

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAMjEvIi1LIMXMxYPfaB6-042hs8bApK2k",
   authDomain: "lyvo-web.firebaseapp.com",
   projectId: "lyvo-web",
-  storageBucket: "lyvo-web.appspot.com",
+  storageBucket: "lyvo-web.firebasestorage.app",
   messagingSenderId: "501648718670",
   appId: "1:501648718670:web:cc5a8e6e2119cc8222bc1f",
 };
