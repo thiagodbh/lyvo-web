b21728f5552b2e46e73f773bd10b566919d4ab08
thiagodbh <thiagodbh@gmail.com>
2026-02-05 01:49:42 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,15 +2,22 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // Novo import
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+// O seu Client ID gerado no Google Cloud
+const CLIENT_ID = "501648718670-u7pc1vj25rfudk3nfo4mnmvhc9tcgeud.apps.googleusercontent.com";
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* O Provider apenas fornece o contexto para o Google, não altera o App atual */}
+    <GoogleOAuthProvider clientId={CLIENT_ID}> 
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
